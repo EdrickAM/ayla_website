493a3007d38a95447f5d467cde4bbcbd43c38b11
Edrick Manoel <edrickmanoel@nasajon.com.br>
2025-02-25 15:28:43 -0300

    Qr Code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -136,3 +136,18 @@ document.addEventListener("click", function (event) {
       checkbox.checked = false;
   }
 });
+
+function openDay(evt, dayName) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab-link");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(dayName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
