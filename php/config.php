4e69c64bbbaa0344862ebe6d43dac6d1efc5ba17
Edrick Manoel <edrickmanoel@nasajon.com.br>
2025-03-06 17:21:11 -0300

    Pixel

--- php/config.php
+++ php/config.php
@@ -1,6 +1,6 @@
 <?php
 // Carrega o autoload do Composer e as variáveis do .env
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
